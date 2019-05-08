663333715ca757e0b7ce3ad5fcd9ab403af4ded2
Bochesan <bochesan89@yandex.ru>
2019-05-08 12:17:50 +0300

    Clean

--- footer.php
+++ footer.php
@@ -1,6 +1,5 @@
 </div>
 
-<script src="/assets/scripts/jquery-3.3.1.min.js"></script>
 <script src="<?=$buildDest?>main.min.js?<?= filemtime($_SERVER['DOCUMENT_ROOT'].$buildDest.'main.min.js'); ?>" defer></script>
 </body>
 </html>
